00df13e26de37e700c16a95a168ad73f7b8fe5bc
Shekhar R <a-8177@LE1125.local>
2020-12-14 19:11:54 +0530

    clean up

--- __tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import SearchView from '../src/views/search-view';
-
-test('renders learn react link', () => {
-  render(<SearchView />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
